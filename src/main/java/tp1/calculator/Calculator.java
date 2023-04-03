9f7fbf9e694bae245154dd60d3ba60cb3d3e184a
Gustavo Marcos Lambertini <marcos.lambertini@ing.austral.edu.ar>
2023-04-03 12:56:15 -0300

    Calculator ToHex

--- src/main/java/tp1/calculator/Calculator.java
+++ src/main/java/tp1/calculator/Calculator.java
@@ -1,4 +1,71 @@
 package tp1.calculator;
 
-public class Calculator {
+import java.util.Stack;
+
+public class Calculator implements tp1.interfaces.Calculator {
+    @Override
+    public String sum(String a, String b) {
+        return null;
+    }
+
+    @Override
+    public String sub(String a, String b) {
+        return null;
+    }
+
+    @Override
+    public String mult(String a, String b) {
+        return null;
+    }
+
+    @Override
+    public String div(String a, String b) {
+        return null;
+    }
+
+    @Override
+    public String toHex(String binary) {
+        int decimal = this.toDecimal(binary);
+        Stack<Integer> stack = new Stack<>();
+        int numerator = decimal;
+        int quotient = numerator/16;
+        int remainder = numerator%16;
+        while(numerator>=16){
+            numerator = quotient;
+            stack.push(remainder);
+            quotient = numerator/16;
+            remainder = numerator%16;
+        }
+        stack.push(remainder);
+        StringBuilder hex = new StringBuilder();
+        while(!stack.isEmpty()){//while stack has numbers
+            int n = stack.pop();
+            switch(n){
+                case 10: hex.append("A");
+                case 11: hex.append("B");
+                case 12: hex.append("C");
+                case 13: hex.append("D");
+                case 14: hex.append("E");
+                case 15: hex.append("F");
+                default: hex.append(n);
+            }
+        }
+        return hex.toString();
+    }
+
+    @Override
+    public String fromHex(String hex) {
+        return null;
+    }
+
+    public Integer toDecimal(String binary){
+        int sum = 0;
+        int j = binary.length()-1;
+        for(int i = 0; i<=j ; i++){
+            char number = binary.charAt(binary.length()-1-i);
+            if(number == '0'){continue;}
+            else{sum = (int) (sum + Math.pow(2, i));}
+        }
+        return sum;
+    }
 }
